0362bc47208ea996b9555a828b36436d9419005d
Jose Luis Díaz Alonso <134778793+JoseLuisDiazDev@users.noreply.github.com>
2025-09-13 15:03:12 +0200

    Es el comit del tailwind.config.js en el cual hemos añadido la opción para escribir texto en vertical en el botón To Go UP

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,22 @@ export const theme = {
     backgroundImage: {
       'backgroundPrincipal': "url('/public/media/ImagenFondo.png')"
     },
+    textShadow: {
+      sm: '1px 1px 2px rgba(0,0,0,0.25)',
+      md: '2px 2px 4px rgba(0,0,0,0.3)',
+      lg: '3px 3px 6px rgba(0,0,0,0.4)',
+    }
   },
 };
-export const plugins = [];
+export const plugins = [
+  function ({addUtilities, theme}) {
+    const newUtilities = {}
+    const shadows = theme('textShadow')
+    for (const key in shadows) {
+      newUtilities[`.text-shadow-${key}`] = {
+        textShadow: shadows[key],
+      }
+    }
+    addUtilities(newUtilities)
+  }
+];
